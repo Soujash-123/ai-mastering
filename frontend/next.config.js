b603cdc0ab187bbde4af01865e93802c83e1804a
SoujashBanerjee <soujash.banerjee@maersk.com>
2026-06-01 22:01:06 +0530

    Fixing Deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
   webpack(config) {
     config.resolve.alias = {
       ...config.resolve.alias,
